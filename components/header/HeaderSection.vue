559a62b6b0c8e5cee559ea766d0d0af8374c7c2a
Aaron Freebern <afreebern@brandxads.com>
2019-06-24 09:12:29 -0400

    adding social icons

--- components/header/HeaderSection.vue
+++ components/header/HeaderSection.vue
@@ -1,20 +1,36 @@
 <template>
   <section>
-    <div>
-      <h1>
-        Hudson River Brewhouse
-      </h1>
-      <div class="sub-header-flex">
-        <h2>Coffee</h2>
-        <h2>Tea</h2>
-        <h2>Craft Beer</h2>
+    <div class="social-icons">
+      <img
+        src="https://res.cloudinary.com/dipgyyv6a/image/upload/v1561141941/hrbh/facebook-bisque_pime4e.svg"
+        alt="facebook"
+      />
+      <img
+        src="https://res.cloudinary.com/dipgyyv6a/image/upload/v1561141941/hrbh/twitter-bisque_wjlz2b.svg"
+        alt="twitter"
+      />
+      <img
+        src="https://res.cloudinary.com/dipgyyv6a/image/upload/v1561141941/hrbh/insta-bisque_sus9xx.svg"
+        alt="instagram"
+      />
+    </div>
+    <div class="section-wrap">
+      <div>
+        <h1>
+          Hudson River Brewhouse
+        </h1>
+        <div class="sub-header-flex">
+          <h2>Coffee</h2>
+          <h2>Tea</h2>
+          <h2>Craft Beer</h2>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-section {
+.section-wrap {
   background-image: url('https://res.cloudinary.com/dipgyyv6a/image/upload/v1561137054/hrbh/beer-wall-three_l7avdf.jpg');
   background-size: cover;
   background-position: 50%;
@@ -52,8 +68,25 @@ h2 {
   margin-bottom: 20px;
 }
 
+.social-icons {
+  margin: 10px 0 0 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 5;
+  img {
+    height: 40px;
+    margin-right: 10px;
+    padding-bottom: 4px;
+    &:hover {
+      height: 50px;
+      cursor: pointer;
+    }
+  }
+}
+
 @media only screen and (min-width: 900px) {
-  section {
+  .section-wrap {
     background-position: fixed;
   }
   h1 {
